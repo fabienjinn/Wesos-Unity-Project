5a0e81b518bd17d0248c61fff5a0b1d057ccc35d
fabienjinn <funhandball@hotmail.com>
2016-04-05 11:17:31 +0200

    Update scene

---
+++ Wesos Video Game/Assets/Script/PS3_JostickControlTest.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PS3_JostickControlTest : MonoBehaviour 
+{
+	public Vector2 joystick_R = Vector2.zero;
+	public Vector2 joystick_L = Vector2.zero;
+	public Vector2 cross_arrows = Vector2.zero;
+
+	public Vector2 R2_L2;
+
+	// Use this for initialization
+	void Start () 
+	{
+		joystick_R = Vector2.zero;
+		joystick_L = Vector2.zero;
+		cross_arrows = Vector2.zero;
+
+		R2_L2 = Vector2.zero;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		joystick_R.x = Input.GetAxis("R_Joystick_H");
+		joystick_R.y = Input.GetAxis("R_Joystick_V");
+
+		joystick_L.x = Input.GetAxis("L_Joystick_H");
+		joystick_L.y = Input.GetAxis("L_Joystick_V");
+
+		cross_arrows.x = Input.GetAxisRaw("Arrows_H");
+		cross_arrows.y = Input.GetAxisRaw("Arrows_V");
+
+		R2_L2.x = Input.GetAxis("R2");
+		R2_L2.y = Input.GetAxis("L2");
+
+	
+	}
+}
---
+++ Wesos Video Game/Assets/Script/PS3_JoystickButtonTest.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PS3_JoystickButtonTest : MonoBehaviour 
+{
+	/*
+	 * Pour fonctionner , MotioninJoy actif + manette XBOX 360 (Si Manette PS3 passez en mode emulation XBOX 360)
+	 * Ensuite dans Unity , configurer les Inputs 
+	 *
+	 *Cross = joystick button 0
+	 */
+
+
+
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Press();
+	
+	}
+
+	void Press()
+	{
+		if(Input.GetButtonUp("Cross"))
+		{
+			Debug.Log (" CROSS Input received !");
+		}
+		else if(Input.GetButtonUp("Circle"))
+		{
+			Debug.Log (" CIRCLE Input received !");
+		}
+		else if(Input.GetButtonUp("Triangle"))
+		{
+			Debug.Log (" TRIANGLE Input received !");
+		}
+		else if(Input.GetButtonUp("Square"))
+		{
+			Debug.Log (" SQUARE Input received !");
+		}
+		else if(Input.GetButtonUp("Start"))
+		{
+			Debug.Log (" START Input received !");
+		}
+		else if(Input.GetButtonUp("Select"))
+		{
+			Debug.Log (" SELECT Input received !");
+		}
+		else if(Input.GetButtonUp("R1"))
+		{
+			Debug.Log (" R1 Input received !");
+		}
+		else if(Input.GetButtonUp("L1"))
+		{
+			Debug.Log (" L1 Input received !");
+		}
+		else if(Input.GetButtonUp("R3"))
+		{
+			Debug.Log (" R3 Input received !");
+		}
+		else if(Input.GetButtonUp("L3"))
+		{
+			Debug.Log (" L3 Input received !");
+		}
+
+		else
+		{
+			return;
+		}
+	}
+}
